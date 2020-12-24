43efbe612a8611e58866c352d24fb06c370bf1d0
ihsan <42092819+ihsanfil@users.noreply.github.com>
2020-12-24 21:47:32 +0300

    first commit

---
+++ ShopApp.WebUI/Controllers/ShopController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopApp.Business.Abstract;
+using ShopApp.Entities;
+using ShopApp.WebUI.Models;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class ShopController : Controller
+    {
+        private IProductService _productService;
+        public ShopController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Product product = _productService.GetProductDetails((int)id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(new ProductDetailsModel () { 
+                 Product = product,
+                 Categories = product.ProductCategories.Select(i => i.Category).ToList()
+            });
+        }
+        public IActionResult List(string category, int page=1)
+        {
+            const int pageSize = 3;
+            return View(new ProductListModel()
+            {
+                PagingInfo = new PagingInfo()
+                {
+                    TotalItems = _productService.GetCountByCategory(category),
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    CurrentCategory = category
+                },
+                Products = _productService.GetProductsByCategory(category , page , pageSize)
+            });
+        }
+
+
+    }
+}
